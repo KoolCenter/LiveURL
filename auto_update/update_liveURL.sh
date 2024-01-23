96c43a740f0bec843d49920f099adf9133370302
KoolCenter <120372062+KoolCenter@users.noreply.github.com>
2024-01-23 15:11:17 +0800

    Update update_liveURL.sh

--- auto_update/update_liveURL.sh
+++ auto_update/update_liveURL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Create the LiveURL directory if it doesn't exist
-mkdir -p "${GITHUB_WORKSPACE}/LiveURL"
+# Assuming the script is being run from the root of your repository
+# and that the 'LiveURL' directory is also at the root of your repository
 
 # Navigate to the LiveURL directory
-cd "${GITHUB_WORKSPACE}/LiveURL"
+cd "${GITHUB_WORKSPACE}/LiveURL" || exit
 
 # Download the M3U files directly into the LiveURL directory
 wget -q -O IPTV.m3u https://raw.githubusercontent.com/YueChan/Live/main/IPTV.m3u
@@ -12,4 +12,3 @@ wget -q -O Global.m3u https://raw.githubusercontent.com/YueChan/Live/main/Global
 
 # Concatenate the files and save as Combined.m3u in the LiveURL directory
 cat IPTV.m3u Global.m3u > Combined.m3u
-
