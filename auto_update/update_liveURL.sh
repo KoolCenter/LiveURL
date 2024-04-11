8949df81f0a056ab19431497ef5996da5b187c05
KoolCenter <120372062+KoolCenter@users.noreply.github.com>
2024-04-11 17:14:33 +0800

    Update update_liveURL.sh

--- auto_update/update_liveURL.sh
+++ auto_update/update_liveURL.sh
@@ -3,8 +3,8 @@
 pwd
 
 # Download the M3U files directly into the LiveURL directory
-wget -q -O IPTV.m3u https://raw.githubusercontent.com/YueChan/Live/main/IPTV.m3u
-wget -q -O Global.m3u https://raw.githubusercontent.com/YueChan/Live/main/Global.m3u
+wget -q -O IPTV.m3u https://raw.githubusercontent.com/drangjchen/IPTV/tree/main/M3U/ipv6.m3u
+wget -q -O Global.m3u https://raw.githubusercontent.com/drangjchen/IPTV/tree/main/M3U/Global.m3u
 
 # Concatenate the files and save as Combined.m3u in the LiveURL directory
 cat IPTV.m3u <(tail -n +2 Global.m3u) > Combined.m3u
